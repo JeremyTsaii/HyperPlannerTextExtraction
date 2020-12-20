e99b496767d6b6f72b19e6553ca8649d46963b29
Jeremy <jetsai@hmc.edu>
2020-12-19 20:08:28 -0800

    Update s3

--- getPresignedUrl/getPresignedUrl.py
+++ getPresignedUrl/getPresignedUrl.py
@@ -3,22 +3,23 @@ import boto3
 from botocore.config import Config
 
 config = Config(s3={"use_accelerate_endpoint": True})
-s3_client = boto3.client('s3', config=config)
+s3 = boto3.client('s3', config=config)
 
 def lambda_handler(event, context):
     BUCKET = 'Bucket Name'
     OBJECT = event['queryStringParameters']['fileName']
     
-    getJson = event['queryStrinParameters']['getJson']
+    getJson = event['queryStringParameters']['getJson']
     
     # Get extracted courses json file
     if getJson == "true":
         try:
-            data = s3.get_object(Bucket=bucket, Key=key)
+            data = s3.get_object(Bucket=BUCKET, Key=OBJECT)
             json_data = json.loads(data['Body'].read().decode('utf-8'))
-        status = "found"
+            status = "found"
         except Exception as e:
             status= "not found"
+            print(e)
         
         body = {'status': status, 'data': {}}
         if status == "found":
@@ -30,8 +31,9 @@ def lambda_handler(event, context):
                 'Access-Control-Allow-Origin': '*'
             },
             'body': json.dumps(body)
+        }
     else: # Get presigned url for user to upload
-        url = s3_client.generate_presigned_url(
+        url = s3.generate_presigned_url(
             'put_object',
             Params={'Bucket': BUCKET, 'Key': OBJECT, 'ContentType': 'application/pdf'},
             ExpiresIn=120)
