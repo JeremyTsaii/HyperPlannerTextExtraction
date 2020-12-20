4e64944eda68c87dfae8c5464a45edfd659a916c
Jeremy <jetsai@hmc.edu>
2020-12-19 19:16:14 -0800

    Add view json option

--- getPresignedUrl/getPresignedUrl.py
+++ getPresignedUrl/getPresignedUrl.py
@@ -9,15 +9,37 @@ def lambda_handler(event, context):
     BUCKET = 'Bucket Name'
     OBJECT = event['queryStringParameters']['fileName']
     
-    url = s3_client.generate_presigned_url(
-        'put_object',
-        Params={'Bucket': BUCKET, 'Key': OBJECT},
-        ExpiresIn=300)
+    getJson = event['queryStrinParameters']['getJson']
     
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': json.dumps(url)
-    }
+    # Get extracted courses json file
+    if getJson == "true":
+        try:
+            data = s3.get_object(Bucket=bucket, Key=key)
+            json_data = json.loads(data['Body'].read().decode('utf-8'))
+        status = "found"
+        except Exception as e:
+            status= "not found"
+        
+        body = {'status': status, 'data': {}}
+        if status == "found":
+            body['data'] = json_data
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps(body)
+    else: # Get presigned url for user to upload
+        url = s3_client.generate_presigned_url(
+            'put_object',
+            Params={'Bucket': BUCKET, 'Key': OBJECT, 'ContentType': 'application/pdf'},
+            ExpiresIn=120)
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps(url)
+        }
