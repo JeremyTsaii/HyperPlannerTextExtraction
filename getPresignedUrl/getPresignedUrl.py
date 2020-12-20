6315a372a08250d467b9d447ca03d2b22965527a
Jeremy <jetsai@hmc.edu>
2020-12-19 16:20:40 -0800

    Add getPresignedUrl

---
+++ getPresignedUrl/getPresignedUrl.py
@@ -0,0 +1,20 @@
+import json
+import boto3
+from botocore.config import Config
+
+def lambda_handler(event, context):
+    config = Config(s3={"use_accelerate_endpoint": True})
+    s3_client = boto3.client('s3', config=config)
+    
+    BUCKET = 'Bucket Name'
+    OBJECT = event['queryStringParameters']['fileName']
+    
+    url = s3_client.generate_presigned_url(
+        'put_object',
+        Params={'Bucket': BUCKET, 'Key': OBJECT},
+        ExpiresIn=300)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(url)
+    }
